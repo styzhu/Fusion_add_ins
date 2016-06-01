d62fe69116a10c25c622f1951e2d6302f2bf1320
StyZhu <zhu@junyuan.me>
2016-06-01 16:33:00 +0800

    some shits

--- BrickBuilder/BrickBuilder.py
+++ BrickBuilder/BrickBuilder.py
@@ -3,12 +3,13 @@
 
 import adsk.core, adsk.fusion, adsk.cam, traceback
 
-app = adsk.core.Application.get()
-ui  = app.userInterface
-commandId = 'BrickBuilder'
-commandName = 'Brick Builder'
-commandDescription = 'Turn a Shape Body into Bricks'
-commandResources = 'resources'
+
+def getPanelById(panelId):
+    workspaces_ = ui.workspaces
+    modelingWorkspace_ = workspaces_.itemById(workspaceToUse)
+    toolbarPanels_ = modelingWorkspace_.toolbarPanels
+    toolbarPanel_ = toolbarPanels_.itemById(panelId)
+    return toolbarPanel_
 
 class MyCommandDestroyHandler(adsk.core.CommandEventHandler):
     def __init__(self):
@@ -22,7 +23,7 @@ class MyCommandDestroyHandler(adsk.core.CommandEventHandler):
             if ui:
                 ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
 
-class MyCommandCreatedHandler(adsk.core.CommandCreatedEventHandler):
+class BuildBrickCommandCreatedHandler(adsk.core.CommandCreatedEventHandler):
     def __init__(self):
         super().__init__()
     def notify(self, args):
@@ -33,7 +34,6 @@ class MyCommandCreatedHandler(adsk.core.CommandCreatedEventHandler):
             # Keep the handler referenced beyond this function
             handlers.append(onDestroy)
             inputs = cmd.commandInputs
-            global commandId
 
             # Create selection input
             selectionInput = inputs.addSelectionInput(commandId + '_shape_selection', 'Select Shape', 'Select the Shape Body')
@@ -47,28 +47,67 @@ class MyCommandCreatedHandler(adsk.core.CommandCreatedEventHandler):
             if ui:
                 ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
 
+def addCommandToPanel(commandId, commandName, commandDescription, commandResources, onCommandCreated):
+    commandDefinitions_ = ui.commandDefinitions
+
+    toolbarControlsPanel_ = getPanelById(panelToUse).controls
+    toolbarControlPanel_ = toolbarControlsPanel_.itemById(commandId)
+    if not toolbarControlPanel_:
+        commandDefinitionPanel_ = commandDefinitions_.itemById(commandId)
+        if not commandDefinitionPanel_:
+            commandDefinitionPanel_ = commandDefinitions_.addButtonDefinition(commandId, commandName, commandDescription, commandResources)
+
+        commandDefinitionPanel_.commandCreated.add(onCommandCreated)
+
+        adsk.core.NamedValues.create()
+
+        # Keep the handler referenced beyond this function
+        handlers.append(onCommandCreated)
+        toolbarControlPanel_ = toolbarControlsPanel_.addCommand(commandDefinitionPanel_, '')
+        toolbarControlPanel_.isVisible = True
+
 def run(context):
     ui = None
     try:
-        UI.run(context)
+        global app
+        global ui
+        global commandId
+        global commandName
+        global commandDescription
+        global commandResources
+        global handlers
+        global workspaceToUse
+        global panelToUse
+        handlers = []
+        commandId = 'BrickBuilder'
+        commandName = 'Brick Builder'
+        commandDescription = 'Turn a Shape Body into Bricks'
+        commandResources = 'resources'
+        app = adsk.core.Application.get()
+        ui  = app.userInterface
+        workspaceToUse = 'FusionSolidEnvironment'
+        panelToUse = 'SolidCreatePanel'
 
-        # Create command defintion
-        cmdDef = ui.commandDefinitions.itemById(commandId)
-        if not cmdDef:
-            cmdDef = ui.commandDefinitions.addButtonDefinition(commandId, commandName, commandDescription)
+        addCommandToPanel(commandId, commandName, commandDescription, commandResources, BuildBrickCommandCreatedHandler())
 
-        # Add command created event
-        onCommandCreated = MyCommandCreatedHandler()
-        cmdDef.commandCreated.add(onCommandCreated)
-        # Keep the handler referenced beyond this function
-        handlers.append(onCommandCreated)
+        ui.messageBox('hello')
 
-        # Execute command
-        cmdDef.execute()
+        # # Create command defintion
+        # cmdDef = ui.commandDefinitions.itemById(commandId)
+        # if not cmdDef:
+        #     cmdDef = ui.commandDefinitions.addButtonDefinition(commandId, commandName, commandDescription)
+        #
+        # # Add command created event
+        # onCommandCreated = MyCommandCreatedHandler()
+        # cmdDef.commandCreated.add(onCommandCreated)
+        # # Keep the handler referenced beyond this function
+        # handlers.append(onCommandCreated)
+        #
+        # # Execute command
+        # cmdDef.execute()
 
         # Prevent this module from being terminate when the script returns, because we are waiting for event handlers to fire
         adsk.autoTerminate(False)
-
     except:
         if ui:
             ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
@@ -76,7 +115,9 @@ def run(context):
 def stop(context):
     ui = None
     try:
-        UI.stop(context)
+        app = adsk.core.Application.get()
+        ui  = app.userInterface
+        ui.messageBox('Stop addin')
     except:
         if ui:
             ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
